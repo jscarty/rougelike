8aa4d2d62f080a83bdb30a95657ead4686bc24cc
jcarty <jscarty@blueyonder.co.uk>
2021-08-05 14:52:10 +0200

    wip

---
+++ enity.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+class Enity:
+    """
+    A generic object to represent players, enemies, items, etc.
+    """
+
+    def __init__(self, x: int, y: int, char: str, color: tuple[int, int, int]):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx: int, dy: int) -> None:
+        # Move the entity by a give amount
+        self.x += dx
+        self.y += dy
